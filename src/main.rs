f845b82a4d86dbc148a638a351935a0ed68272fc
ashtneoi <ashtneoi@gmail.com>
2021-02-27 15:25:45 -0800

    Implement get_ngrams

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,40 @@
+use std::fs::File;
+use std::io::{prelude::*, BufReader};
+
+fn get_ngrams(maxlen: usize) -> Vec<Vec<(String, u64)>> {
+    let mut n = Vec::new();
+    let f = File::open("ngrams-all.tsv").unwrap();
+    let mut lines = BufReader::new(f);
+    let mut line = String::new();
+    for i in 1..=maxlen {
+        if line.is_empty() {
+            lines.read_line(&mut line).unwrap();
+        }
+        assert!(!line.is_empty());
+        let fields: Vec<_> = line.splitn(3, '\t').collect();
+        let kind = fields[0];
+        let splats = fields[1];
+        assert_eq!(kind, &format!("{}-gram", i));
+        assert_eq!(splats, "*/*");
+        line.clear();
+        let mut igrams = Vec::new();
+        while lines.read_line(&mut line).unwrap() > 0 {
+            let fields: Vec<_> = line.splitn(3, '\t').collect();
+            let igram = fields[0];
+            if igram.ends_with("-gram") {
+                // Don't clear line (it's the next header).
+                break;
+            }
+            let count = fields[1];
+            igrams.push((igram.to_string(), count.parse().unwrap()));
+            line.clear();
+        }
+        n.push(igrams);
+    }
+
+    n
+}
+
 fn main() {
-    println!("Hello, world!");
+    println!("{:?}", get_ngrams(2));
 }
